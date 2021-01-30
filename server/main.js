73defaed99c6e5b043ad3e4e2df38b82da255118
psvnlsaikumar <psvnlsaikumar@gmail.com>
2021-01-30 16:46:38 +0530

    added paysafe

--- server/main.js
+++ server/main.js
@@ -1,5 +1,13 @@
-import { Meteor } from 'meteor/meteor';
+import { Meteor } from "meteor/meteor";
 
 Meteor.startup(() => {
-  // code to run on server at startup
+  process.env.API_KEY = ``;
+});
+
+Meteor.methods({
+  async getAPIKEY() {
+    new Promise((resolve, reject) => {
+      resolve(process.env.API_KEY);
+    });
+  },
 });
